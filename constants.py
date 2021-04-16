654e2567f5a401c9765463b1023a5d125e7f6e77
8ayac <8ayac.y@gmail.com>
2021-04-17 03:15:41 +0900

    Implement `!nominator` command.

--- constants.py
+++ constants.py
@@ -6,8 +6,12 @@ BOT_TOKEN = os.environ.get('BOT_TOKEN')
 
 COMMAND_PREFIX = '!'
 INITIAL_EXTENSIONS = [
-    'cogs.historian'
+    'cogs.historian',
+    'cogs.nominator'
 ]
 
 # 生成したMarkdownを保存するディレクトリ
 MD_DIR = join(dirname(__file__), 'mdtmp')
+
+# nominatorコマンドで指名できる人数の上限
+NOMINATOR_LIMIT = 50
